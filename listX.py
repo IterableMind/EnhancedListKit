d1c5f4425aee16a0a8056084a2055dd7ab0c1ffb
IterableMind <142767580+IterableMind@users.noreply.github.com>
2023-08-23 20:28:15 +0300

    Update listX.py

--- listX.py
+++ listX.py
@@ -1,4 +1,3 @@
-from collections.abc import Iterable as _Iterable
 import types
 import string
 
@@ -12,51 +11,49 @@ _FUNCTIONSTYPES = [
 _STRIP_CHARS = list(string.whitespace + string.punctuation)
 
 def argtype(*args):
+    """
+    Check if the first two arguments are integers.
+    """
     return all(isinstance(arg, int) for arg in args[:2])
 
 class FunctionalList(list):
     """
     Extends Python's list with a cleaner interface for certain practices.
     """
-    def allmatch(self, callbackfn):
-        """
-        Returns True if every element in the list satisfies the callback function.
-        """
-        vals = [ch for ch in self if ch not in _STRIP_CHARS]
-        res = list(filter(callbackfn, vals))
-        if self and isinstance(callbackfn(vals[0]), bool):
-            return len(vals) == len(res)
-        return False
 
-    def modify_range(self, *args):
+    def all_match(self, callback):
+        """
+        Returns True if every element in the list satisfies the callback
+        function else False.
+        """
+        elements = [c for c in self if c not in _STRIP_CHARS]
+        if self and isinstance(callback(elements[0]), bool):
+            return len(elements) == len([v for v in filter(callback, elements)])
+        return False  # Empty list
+
+    def splice(self, startindex, delcount, *items):
         """
         Modifies a range of elements within the list by removing and inserting items.
-        :param: arg 1 must be an integer specifying where to start removing
-                element(s).
-        :param: arg 2 must be an integer specifying the number of elements to remove.
-        :param: *args => Any number of elements to be inserted.
         """
-        argslen = len(args)
-        if argslen == 1:
-            if argtype(*args):
-                self.pop(args[0])
-                return
-            raise TypeError("arg 1 must be an int")
-        if argslen >= 2:
-            startindex, delcount, *items = args
-            if argtype(*args):
-                if items:
-                    self[startindex: startindex + delcount] = [i for i in items]
-                    return
-                self[startindex: startindex + delcount] = []
-                return
-            raise TypeError("arg 1 and 2 must be ints")
+        if not argtype(startindex, delcount):
+            raise TypeError('startindex and delcount must be integers')
 
-    def foreach(self, callbackfn):
+        if startindex >= len(self):
+            raise IndexError('startindex is out of range')
+
+        if len(self) < delcount:
+            raise IndexError(f'Trying to remove {delcount} elements but only {len(self)} present')
+
+        if items:
+            self[startindex: startindex + delcount] = list(items)
+        else:
+            self[startindex: startindex + delcount] = []
+
+    def for_every(self, callback):
         """
         Applies a function to each item in the list and returns a new processed list.
         """
-        arg = type(callbackfn)
-        if arg in _FUNCTIONSTYPES:
-            return [callbackfn(item) for item in self]
-        raise TypeError("arg 1 must be a function")
+        if isinstance(callback, tuple(_FUNCTIONSTYPES)):
+            return [callback(item) for item in self]
+        raise TypeError('arg 1 must be a function')
+
